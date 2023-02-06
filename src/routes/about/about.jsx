764986c3c8ec6ab1473dd9783fd20e5374419c65
Igors Koptevs <igors.koptevs@gmail.com>
2023-02-06 09:36:03 +0200

    added info par kabini

--- src/routes/about/about.jsx
+++ src/routes/about/about.jsx
@@ -1,7 +1,22 @@
 import React from "react"
 
 const About = () => {
-    return <div>About</div>
+    return (
+        <>
+            <div>About</div>
+            <h3>
+                Kabīnē redzamā vietā novietota plāksnīte, kurā skaidri norādīta
+                šāda informācija:
+            </h3>
+            <ul>
+                <li>45.1. lifta ražotāja vai uzstādītāja nosaukums;</li>
+                <li>45.2. lifta modeļa, sērijas vai tipa apzīmējums;</li>
+                <li>45.3. lifta uzstādīšanas gads;</li>
+                <li>45.4. lifta nominālā celtspēja kilogramos;</li>
+                <li>45.5. maksimālais pasažieru skaits.</li>
+            </ul>
+        </>
+    )
 }
 
 export default About
