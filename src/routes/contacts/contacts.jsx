6e61e5e26ceaa26d789dcad644a32f7820e1cd81
Igors Koptevs <igors.koptevs@gmail.com>
2023-03-10 22:06:39 +0200

    Testing link

--- src/routes/contacts/contacts.jsx
+++ src/routes/contacts/contacts.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 
 const Contacts = () => {
-    return <div>Contacts</div>
+    return (
+        <>
+            <div>Contacts</div>
+            <a href="/contacts/123">123</a>
+        </>
+    )
 }
 
 export default Contacts
